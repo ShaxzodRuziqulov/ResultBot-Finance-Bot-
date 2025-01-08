3804b6444f92e5279fc335b85d76529f97e29260
Shaxzod Ruziqulov <janobprizident@gmail.com>
2025-01-08 12:57:39 +0500

    first commit

---
+++ src/main/java/com/example/resultbot/ResultBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.resultbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultBotApplication.class, args);
+	}
+
+}
